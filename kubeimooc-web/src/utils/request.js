87c14f134bf9d39454a287b363c0fc7e4ab32c0b
高桥琳介 <18574945291@163.com>
2023-12-11 23:13:40 +0800

    前后端联调

--- kubeimooc-web/src/utils/request.js
+++ kubeimooc-web/src/utils/request.js
@@ -44,9 +44,9 @@ service.interceptors.response.use(
    */
   response => {
     const res = response.data
-
+    console.log("gan",res.code)
     // if the custom code is not 20000, it is judged as an error.
-    if (res.code) {
+    if (res.code !== 200) {
       Message({
         message: res.msg || 'Error',
         type: 'error',
